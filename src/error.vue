e0b05aed6c90352af8bc9a71a095ecf6ae0e0c79
wajeht <58354193+wajeht@users.noreply.github.com>
2023-12-09 22:11:32 -0600

    feat: Add error page component

---
+++ src/error.vue
@@ -0,0 +1,25 @@
+<script setup>
+const isDev = process.dev
+const props = defineProps({ error: Object })
+const message = computed(() => String(props.error?.message || ''))
+const is404 = computed(() => props.error?.statusCode === 404 || message.value?.includes('404'))
+const language = computed(() => is404 ? 'This page could not be found' : 'An error occurred');
+
+</script>
+
+<template>
+  <NuxtLayout>
+    <div class="flex flex-col h-screen text-center items-center justify-center gap-4">
+
+      <div class="text-3xl"> {{ language }} </div>
+
+      <div class="text-xl opacity-50">
+        Looks like you've followed a broken link or entered a URL that doesn't exist on this site.
+      </div>
+
+      <UButton @click="$router.back()" color="primary" variant="outline" label="Go back" :trailing="false" />
+
+      <pre v-if="isDev">{{ error }}</pre>
+    </div>
+  </NuxtLayout>
+</template>
