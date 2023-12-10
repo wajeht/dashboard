f5530efc0298d1336cd6d1bd1d65aad5afe6690a
wajeht <58354193+wajeht@users.noreply.github.com>
2023-12-09 19:01:27 -0600

    feat: Add @nuxtjs/robots and @nuxtjs/plausible dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: './src',
   devtools: { enabled: true },
+  robots: {
+     UserAgent: '*',
+     Disallow: '/' ,
+  },
+  plausible: {
+    apiHost: 'https://plausible.jaw.dev/',
+ },
   modules: [
     "@nuxtjs/tailwindcss",
     "@formkit/auto-animate",
     "@vueuse/nuxt",
     "@nuxt/image",
     "@nuxtjs/color-mode",
-    "nuxt-vitest"
+    "nuxt-vitest",
+    "@nuxtjs/robots",
+    "@nuxtjs/plausible"
   ]
 })
