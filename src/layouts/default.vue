815b06736c660e8aedc0f7ca868ef522f0e2b666
wajeht <58354193+wajeht@users.noreply.github.com>
2023-12-10 01:00:16 -0600

    feat: Add default layout and tailwind config

---
+++ src/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-fade-pattern bg-cover">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+				'fade-pattern': 'url("/assets/images/background.png")',
+			},
+    },
+  },
+  plugins: [],
+}
