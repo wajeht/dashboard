ab6ee11674f263db7e7761e510f00f704319cd77
Jaw <58354193+wajeht@users.noreply.github.com>
2023-12-11 13:45:04 -0600

    refactor: auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,15 @@
-export default defineNuxtRouteMiddleware(() => {
-  const { loggedIn } = useUserSession();
+export default defineNuxtRouteMiddleware(async () => {
+  const { loggedIn, clear } = useUserSession()
+  const config = useRuntimeConfig();
+
+  if (config.adminEmailAddress !== user.email) {
+    await clear();
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Unauthorized',
+    });
+  }
 
   if (!loggedIn.value)
-    return navigateTo('/');
-})
+    return navigateTo('/')
+});
--- src/server/api/auth/google.get.ts
+++ src/server/api/auth/google.get.ts
@@ -1,13 +1,5 @@
 export default oauth.googleEventHandler({
   async onSuccess(event, { user }) {
-    const config = useRuntimeConfig(event);
-
-    if (config.adminEmailAddress !== user.email) {
-      throw createError({
-        statusCode: 401,
-        statusMessage: 'Unauthorized',
-      });
-    }
     await setUserSession(event, { user: { google: user } });
     return sendRedirect(event, '/');
   },
