59870c59600f490eb0f8e0257f4f7bd6cdda91df
Jaw <58354193+wajeht@users.noreply.github.com>
2023-12-11 14:09:45 -0600

    refactor: start script

--- src/server/database/db.ts
+++ src/server/database/db.ts
@@ -1,17 +1,19 @@
-import { PrismaClient } from '@prisma/client'
+/* eslint-disable vars-on-top */
+/* eslint-disable node/prefer-global/process */
+import { PrismaClient } from '@prisma/client';
 
 declare global {
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
+  var prisma: PrismaClient | undefined;
 }
 
 const prisma
-	= global.prisma
+	= globalThis.prisma
 	|| new PrismaClient({
 	  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-	})
+	});
 
 if (process.env.NODE_ENV !== 'production')
-  global.prisma = prisma
+  globalThis.prisma = prisma;
 
-export default prisma
+export default prisma;
